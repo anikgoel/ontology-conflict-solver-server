00ab64d0800a908785317eea4894bbf78c5231e5
Kajal Dhiman <kajaldhiman@sabiovision.com>
2022-08-19 12:13:48 +0530

    resolve the issue of show classes

--- dev/DailyOperations/ShowClasses.php
+++ dev/DailyOperations/ShowClasses.php
@@ -4,8 +4,25 @@ require_once '../../includes/DataBaseOperations.php';
 
     if($_SERVER['REQUEST_METHOD'] == 'GET')
     {
-        $db = new DataBaseOperations();
+       /* $db = new DataBaseOperations();
         $data = $db->sendClassesData($_GET['type']);
-        echo $data;      
+        $data = preg_replace('/\s+/', '', $data);
+        echo $data;      */
+
+        if($_GET['type'] == 1) {
+            $url = "http://shark.sbs.arizona.edu:8080/carex/getSubclasses?baseIri=http://biosemantics.arizona.edu/ontologies/carex&term=anatomical%20structure";
+            $ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL, $url);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        $data = curl_exec($ch);
+	        echo $data;
+        }else if($_GET['type'] == 0) {
+            $url = "http://shark.sbs.arizona.edu:8080/carex/getSubclasses?baseIri=http://biosemantics.arizona.edu/ontologies/carex&term=quality";
+            $ch = curl_init();
+	        curl_setopt($ch, CURLOPT_URL, $url);
+	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	        $data = curl_exec($ch);
+	        echo $data;
+        }
     }
 ?>
